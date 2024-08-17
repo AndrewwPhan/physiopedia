909c3ace613641655f707e05fbc8a81d65b8851c
Andrew Phan <aphan678@gmail.com>
2024-08-17 21:09:10 +1000

    Updated GUI

--- physiopedia/app.py
+++ physiopedia/app.py
@@ -4,19 +4,19 @@ import streamlit as st
 
 def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
     # db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-    db_uri = "sqlite:///test.db"
+    db_uri = "sqlite:///gym.db"
     print(db_uri)
     return SQLDatabase.from_uri(db_uri)
 
 def main():
     load_dotenv()
 
-    st.set_page_config(page_title="Chat with my SQL", page_icon=":speech_balloon:")
-    st.title("Chat with MYSQL")
+    st.set_page_config(page_title="Chat with PhysioPedia", page_icon=":speech_balloon:")
+    st.title("Chat with PhysioPedia")
 
     with st.sidebar:
         st.subheader("Settings")
-        st.write("This is a simple chat application using MySQL. Connect to the database and start chatting.")
+        st.write("This is a simple chat application using PhysioPedia. Connect to the database and start chatting.")
 
         st.text_input("Host", value="localhost", key="Host")
         st.text_input("Port", value = "3306", key="Port")
@@ -36,7 +36,7 @@ def main():
                 st.session_state.db = db
                 st.success("Connected to database!")
 
-        st.chat_input("Type a message...")
+    st.chat_input("Type a message...")
 
 
 if __name__ == "__main__":
